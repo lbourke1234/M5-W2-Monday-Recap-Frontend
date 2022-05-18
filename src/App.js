9ed7ec3138526765e35a52f25f0a040cdb00cd87
lbourke1234 <lbourke1234@gmail.com>
2022-05-18 15:32:03 +0100

    push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import React from "react";
-import NavBar from "./components/navbar/BlogNavbar";
-import Footer from "./components/footer/Footer";
-import Home from "./views/home/Home";
-import Blog from "./views/blog/Blog";
-import NewBlogPost from "./views/new/New";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from 'react'
+import NavBar from './components/navbar/BlogNavbar'
+import Footer from './components/footer/Footer'
+import Home from './views/home/Home'
+import Blog from './views/blog/Blog'
+import NewBlogPost from './views/new/New'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
+  const baseUrl = process.env.REACT_APP_FE_URL
+
   return (
     <Router>
       <NavBar />
       <Routes>
-        <Route path="/" exact element={<Home />} />
+        <Route
+          path="https://m5-w2-monday-recap-frontend.vercel.app/"
+          exact
+          element={<Home />}
+        />
         <Route path="/blog/:id" element={<Blog />} />
         <Route path="/new" element={<NewBlogPost />} />
       </Routes>
       <Footer />
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
